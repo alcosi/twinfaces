516b4ce810d83f8512975e88fc7f10548073c08b
pihaizer <malhin.vladimir@gmail.com>
2024-09-13 16:12:50 +0200

    [TWINFACES-31] Transition triggers table and create/edit triggers

---
+++ src/app/twinclass/[twinClassId]/twinflow/[twinflowId]/transition/[transitionId]/twinflow-transition-validators.tsx
@@ -0,0 +1,5 @@
+import {TwinFlowTransition} from "@/lib/api/api-types";
+
+export function TwinflowTransitionValidators({transition, onChange}: { transition: TwinFlowTransition, onChange: () => any }) {
+    return <></>
+}
