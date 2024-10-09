e37815e7c671d362d39bbb3b918a70a2f1904fa8
pihaizer <malhin.vladimir@gmail.com>
2024-10-09 14:51:22 +0200

    Fix mapToChoice when input is undefined

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -1,8 +1,8 @@
 type ChoiceType = "ONLY" | "ONLY_NOT" | "ANY";
 type TriStateFlag = boolean | "indeterminate"
 
-export const mapToChoice = (input: TriStateFlag): ChoiceType => {
-    if (input === 'indeterminate') return "ANY";
+export const mapToChoice = (input?: TriStateFlag): ChoiceType => {
+    if (input === 'indeterminate' || input === undefined) return "ANY";
 
     return input ? "ONLY" : "ONLY_NOT";
 }
