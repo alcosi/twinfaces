50cb2a026ddc806e9bf05b32fc47eba5112887b9
Kerim Berdimyradov <k.berdimyradov@alcosi.eu>
2024-10-12 18:02:24 +0300

    :rotating_light: Fix linter issues at 'src/lib/**/*'

--- src/lib/api/api-groups/twin-class-api.ts
+++ src/lib/api/api-groups/twin-class-api.ts
@@ -1,175 +1,218 @@
-import {ApiSettings, getApiDomainHeaders} from "@/lib/api/api";
-import {PaginationState} from "@tanstack/table-core";
+import { ApiSettings, getApiDomainHeaders } from "@/lib/api/api";
+import { PaginationState } from "@tanstack/table-core";
 import {
-    TwinClassCreateRq,
-    TwinClassFieldCreateRq,
-    TwinClassFieldUpdateRq,
-    TwinClassUpdateRq
+  TwinClassCreateRq,
+  TwinClassFieldCreateRq,
+  TwinClassFieldUpdateRq,
+  TwinClassUpdateRq,
 } from "@/lib/api/api-types";
-import {components, operations} from "@/lib/api/generated/schema";
+import { components, operations } from "@/lib/api/generated/schema";
 
-type TwinClassApiFilters = Partial<Pick<components["schemas"]["TwinClassListRqV1"],
-    | 'twinClassIdList'
-    | 'twinClassKeyLikeList'
-    | 'nameI18nLikeList'
-    | 'descriptionI18nLikeList'
-    | 'headTwinClassIdList'
-    | 'extendsTwinClassIdList'
-    | 'ownerTypeList'
-    | 'twinflowSchemaSpace'
-    | 'twinClassSchemaSpace'
-    | 'permissionSchemaSpace'
-    | 'aliasSpace'
-    | 'abstractt'
->>
+type TwinClassApiFilters = Partial<
+  Pick<
+    components["schemas"]["TwinClassListRqV1"],
+    | "twinClassIdList"
+    | "twinClassKeyLikeList"
+    | "nameI18nLikeList"
+    | "descriptionI18nLikeList"
+    | "headTwinClassIdList"
+    | "extendsTwinClassIdList"
+    | "ownerTypeList"
+    | "twinflowSchemaSpace"
+    | "twinClassSchemaSpace"
+    | "permissionSchemaSpace"
+    | "aliasSpace"
+    | "abstractt"
+  >
+>;
 
 export function createTwinClassApi(settings: ApiSettings) {
+  function search({
+    pagination,
+    search,
+    filters,
+  }: {
+    pagination: PaginationState;
+    search?: string;
+    filters: TwinClassApiFilters;
+  }) {
+    return settings.client.POST("/private/twin_class/search/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        query: {
+          showTwinClassMode: "DETAILED",
+          showTwinClassExtends2TwinClassMode: "SHORT",
+          limit: pagination.pageSize,
+          offset: pagination.pageIndex * pagination.pageSize,
+        },
+      },
+      body: {
+        twinClassKeyLikeList: search ? ["%" + search + "%"] : undefined,
+        ...filters,
+      },
+    });
+  }
 
-    function search({
-        pagination,
-        search, 
-        filters,
-    }: {
-        pagination: PaginationState,
-        search?: string,
-        filters: TwinClassApiFilters
-    }) {
-        return settings.client.POST('/private/twin_class/search/v1', {
-            params: {
-                header: getApiDomainHeaders(settings),
-                query: {
-                    showTwinClassMode: 'DETAILED',
-                    showTwinClassExtends2TwinClassMode: 'SHORT',
-                    limit: pagination.pageSize,
-                    offset: pagination.pageIndex * pagination.pageSize
-                }
-            },
-            body: {
-                twinClassKeyLikeList: search ? ['%' + search + '%'] : undefined,
-                ...filters,
-            }
-        })
-    }
+  function getByKey({
+    key,
+    query = {},
+  }: {
+    key: string;
+    query: operations["twinClassViewByKeyV1"]["parameters"]["query"];
+  }) {
+    return settings.client.GET(`/private/twin_class_by_key/{twinClassKey}/v1`, {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinClassKey: key },
+        query: query,
+      },
+    });
+  }
 
-    function getByKey({key, query = {}}: {
-        key: string,
-        query: operations['twinClassViewByKeyV1']['parameters']['query']
-    }) {
-        return settings.client.GET(`/private/twin_class_by_key/{twinClassKey}/v1`, {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassKey: key},
-                query: query
-            }
-        })
-    }
+  function getById({
+    id,
+    query = {},
+  }: {
+    id: string;
+    query: operations["twinClassViewV1"]["parameters"]["query"];
+  }) {
+    return settings.client.GET(`/private/twin_class/{twinClassId}/v1`, {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinClassId: id },
+        query: query,
+      },
+    });
+  }
 
-    function getById({id, query = {}}: { id: string, query: operations['twinClassViewV1']['parameters']['query'] }) {
-        return settings.client.GET(`/private/twin_class/{twinClassId}/v1`, {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassId: id},
-                query: query
-            }
-        })
-    }
+  function getFields({ id }: { id: string }) {
+    return settings.client.GET(
+      `/private/twin_class/{twinClassId}/field/list/v1`,
+      {
+        params: {
+          header: getApiDomainHeaders(settings),
+          query: {
+            showTwinClassFieldMode: "MANAGED",
+            showTwinClass2TwinClassFieldMode: "MANAGED",
+          },
+          path: { twinClassId: id },
+        },
+      }
+    );
+  }
 
-    function getFields({id}: { id: string }) {
-        return settings.client.GET(`/private/twin_class/{twinClassId}/field/list/v1`, {
-            params: {
-                header: getApiDomainHeaders(settings),
-                query: {
-                    showTwinClassFieldMode: "MANAGED",
-                    showTwinClass2TwinClassFieldMode: "MANAGED",
-                },
-                path: {twinClassId: id}
-            }
-        })
-    }
+  function create({ body }: { body: TwinClassCreateRq }) {
+    return settings.client.POST("/private/twin_class/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+      },
+      body: body,
+    });
+  }
 
-    function create({body}: { body: TwinClassCreateRq }) {
-        return settings.client.POST('/private/twin_class/v1', {
-            params: {
-                header: getApiDomainHeaders(settings),
-            },
-            body: body
-        })
-    }
+  function update({ id, body }: { id: string; body: TwinClassUpdateRq }) {
+    return settings.client.PUT("/private/twin_class/{twinClassId}/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinClassId: id },
+      },
+      body: body,
+    });
+  }
 
-    function update({id, body}: { id: string, body: TwinClassUpdateRq }) {
-        return settings.client.PUT('/private/twin_class/{twinClassId}/v1', {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassId: id}
-            },
-            body: body
-        })
-    }
+  function createField({
+    id,
+    body,
+  }: {
+    id: string;
+    body: TwinClassFieldCreateRq;
+  }) {
+    return settings.client.POST(`/private/twin_class/{twinClassId}/field/v1`, {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: {
+          twinClassId: id,
+        },
+      },
+      body: body,
+    });
+  }
 
-    function createField({id, body}: { id: string, body: TwinClassFieldCreateRq }) {
-        return settings.client.POST(`/private/twin_class/{twinClassId}/field/v1`, {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {
-                    twinClassId: id
-                },
-            },
-            body: body
-        })
-    }
+  function updateField({
+    fieldId,
+    body,
+  }: {
+    fieldId: string;
+    body: TwinClassFieldUpdateRq;
+  }) {
+    return settings.client.PUT(
+      "/private/twin_class_field/{twinClassFieldId}/v1",
+      {
+        params: {
+          header: getApiDomainHeaders(settings),
+          path: { twinClassFieldId: fieldId },
+        },
+        body,
+      }
+    );
+  }
 
-    function updateField({fieldId, body}: { fieldId: string, body: TwinClassFieldUpdateRq }) {
-        return settings.client.PUT('/private/twin_class_field/{twinClassFieldId}/v1', {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassFieldId: fieldId}
-            },
-            body
-        })
-    }
+  function getStatusById({ twinStatusId }: { twinStatusId: string }) {
+    return settings.client.GET(`/private/twin_status/{twinStatusId}/v1`, {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinStatusId },
+        query: {
+          showStatusMode: "DETAILED",
+        },
+      },
+    });
+  }
 
-    function getStatusById({ twinStatusId }: { twinStatusId: string }) {
-        return settings.client.GET(`/private/twin_status/{twinStatusId}/v1`, {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: { twinStatusId },
-                query: {
-                    showStatusMode: "DETAILED",
-                }
-            }
-        });
-    }
+  function getFieldById({ fieldId }: { fieldId: string }) {
+    return settings.client.GET(
+      "/private/twin_class_field/{twinClassFieldId}/v1",
+      {
+        params: {
+          header: getApiDomainHeaders(settings),
+          path: { twinClassFieldId: fieldId },
+          query: {
+            showLinkDst2TwinClassMode: "MANAGED",
+            showTwin2StatusMode: "DETAILED",
+            showTwin2TwinClassMode: "MANAGED",
+            showTwin2UserMode: "DETAILED",
+            showTwinAliasMode: "DETAILED",
+            showTwinByHeadMode: "WHITE",
+            showTwinClass2LinkMode: "DETAILED",
+            showTwinClass2StatusMode: "DETAILED",
+            showTwinClass2TwinClassFieldMode: "MANAGED",
+            showTwinClassExtends2TwinClassMode: "MANAGED",
+            showTwinClassFieldDescriptor2DataListOptionMode: "DETAILED",
+            showTwinClassFieldDescriptor2TwinMode: "DETAILED",
+            showTwinClassFieldDescriptor2UserMode: "DETAILED",
+            showTwinClassFieldMode: "MANAGED",
+            showTwinClassHead2TwinClassMode: "MANAGED",
+            showTwinClassMarker2DataListOptionMode: "DETAILED",
+            showTwinClassMode: "MANAGED",
+            showTwinClassTag2DataListOptionMode: "DETAILED",
+          },
+        },
+      }
+    );
+  }
 
-    function getFieldById({fieldId}: { fieldId: string }) {
-        return settings.client.GET("/private/twin_class_field/{twinClassFieldId}/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassFieldId: fieldId},
-                query: {
-                    showLinkDst2TwinClassMode: "MANAGED",
-                    showTwin2StatusMode: "DETAILED",
-                    showTwin2TwinClassMode: "MANAGED",
-                    showTwin2UserMode: "DETAILED",
-                    showTwinAliasMode: "DETAILED",
-                    showTwinByHeadMode: "WHITE",
-                    showTwinClass2LinkMode: "DETAILED",
-                    showTwinClass2StatusMode: "DETAILED",
-                    showTwinClass2TwinClassFieldMode: "MANAGED",
-                    showTwinClassExtends2TwinClassMode: "MANAGED",
-                    showTwinClassFieldDescriptor2DataListOptionMode: "DETAILED",
-                    showTwinClassFieldDescriptor2TwinMode: "DETAILED",
-                    showTwinClassFieldDescriptor2UserMode: "DETAILED",
-                    showTwinClassFieldMode: "MANAGED",
-                    showTwinClassHead2TwinClassMode: "MANAGED",
-                    showTwinClassMarker2DataListOptionMode: "DETAILED",
-                    showTwinClassMode: "MANAGED",
-                    showTwinClassTag2DataListOptionMode: "DETAILED"
-                }
-            }
-        })
-    }
-
-    return {search, getByKey, getById, getFields, getFieldById, create, update, createField, updateField, getStatusById}
+  return {
+    search,
+    getByKey,
+    getById,
+    getFields,
+    getFieldById,
+    create,
+    update,
+    createField,
+    updateField,
+    getStatusById,
+  };
 }
 
 export type TwinClassApi = ReturnType<typeof createTwinClassApi>;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import {forwardRef, ReactNode, Ref, RefAttributes} from "react";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { forwardRef, ReactNode, Ref, RefAttributes } from "react";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
+/* eslint-disable no-unused-vars */
 export function fixedForwardRef<T, P = {}>(
-    render: (props: P, ref: Ref<T>) => ReactNode,
+  render: (props: P, ref: Ref<T>) => ReactNode
 ): (props: P & RefAttributes<T>) => ReactNode {
   return forwardRef(render) as any;
 }
+/* eslint-enable no-unused-vars */
 
-export const NULLIFY_UUID_VALUE: string = 'ffffffff-ffff-ffff-ffff-ffffffffffff'
+export const NULLIFY_UUID_VALUE: string =
+  "ffffffff-ffff-ffff-ffff-ffffffffffff";
