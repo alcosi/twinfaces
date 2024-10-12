0beb8be445da0edfd23fbe6434a9d164ac2c4def
Kerim Berdimyradov <k.berdimyradov@alcosi.eu>
2024-10-12 18:02:24 +0300

    :rotating_light: Fix linter issues at 'src/lib/**/*'

--- src/lib/api/api-groups/featurer-api.ts
+++ src/lib/api/api-groups/featurer-api.ts
@@ -1,27 +1,33 @@
-import {ApiSettings, getApiDomainHeaders} from "@/lib/api/api";
-import {PaginationState} from "@tanstack/table-core";
-import {operations} from "@/lib/api/generated/schema";
-
+import { ApiSettings, getApiDomainHeaders } from "@/lib/api/api";
+import { PaginationState } from "@tanstack/table-core";
+import { operations } from "@/lib/api/generated/schema";
 
 export function createFeaturerApi(settings: ApiSettings) {
-    type SearchOptions = operations["featurerListV1"]["requestBody"]["content"]["application/json"]
+  type SearchOptions =
+    operations["featurerListV1"]["requestBody"]["content"]["application/json"];
 
-    function search({pagination, options}: { pagination: PaginationState, options: SearchOptions }) {
-        return settings.client.POST('/private/featurer/v1', {
-            params: {
-                header: getApiDomainHeaders(settings),
-                query: {
-                    limit: pagination.pageSize,
-                    offset: pagination.pageIndex * pagination.pageSize,
-                    showFeaturerMode: 'DETAILED',
-                    showFeaturerParamMode: 'SHOW',
-                }
-            },
-            body: options
-        })
-    }
+  function search({
+    pagination,
+    options,
+  }: {
+    pagination: PaginationState;
+    options: SearchOptions;
+  }) {
+    return settings.client.POST("/private/featurer/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        query: {
+          limit: pagination.pageSize,
+          offset: pagination.pageIndex * pagination.pageSize,
+          showFeaturerMode: "DETAILED",
+          showFeaturerParamMode: "SHOW",
+        },
+      },
+      body: options,
+    });
+  }
 
-    return {search}
+  return { search };
 }
 
 export type FeaturerApi = ReturnType<typeof createFeaturerApi>;
--- src/lib/api/api-groups/twinflow-api.ts
+++ src/lib/api/api-groups/twinflow-api.ts
@@ -1,132 +1,172 @@
-import {ApiSettings, getApiDomainHeaders} from "@/lib/api/api";
+import { ApiSettings, getApiDomainHeaders } from "@/lib/api/api";
 import {
-    TwinFlowCreateRq,
-    TwinFlowTransitionCreateRq,
-    TwinFlowTransitionUpdateRq,
-    TwinFlowUpdateRq
+  TwinFlowCreateRq,
+  TwinFlowTransitionCreateRq,
+  TwinFlowTransitionUpdateRq,
+  TwinFlowUpdateRq,
 } from "@/lib/api/api-types";
-import {PaginationState} from "@tanstack/table-core";
+import { PaginationState } from "@tanstack/table-core";
 
 export function createTwinflowApi(settings: ApiSettings) {
-    // function create({twinClassId, data}: { twinClassId: string, data: TwinClassStatusCreateRq }) {
-    //     return settings.client.POST('/private/twin_class/{twinClassId}/twin_status/v1', {
-    //         params: {
-    //             header: getApiDomainHeaders(settings),
-    //             path: {twinClassId: twinClassId}
-    //         },
-    //         body: data
-    //     })
-    // }
+  // function create({twinClassId, data}: { twinClassId: string, data: TwinClassStatusCreateRq }) {
+  //     return settings.client.POST('/private/twin_class/{twinClassId}/twin_status/v1', {
+  //         params: {
+  //             header: getApiDomainHeaders(settings),
+  //             path: {twinClassId: twinClassId}
+  //         },
+  //         body: data
+  //     })
+  // }
 
-    // function update({statusId, data}: { statusId: string, data: TwinClassStatusUpdateRq }) {
-    //     return settings.client.PUT('/private/twin_status/{twinStatusId}/v1', {
-    //         params: {
-    //             header: getApiDomainHeaders(settings),
-    //             path: {twinStatusId: statusId}
-    //         },
-    //         body: data
-    //     })
-    // }
+  // function update({statusId, data}: { statusId: string, data: TwinClassStatusUpdateRq }) {
+  //     return settings.client.PUT('/private/twin_status/{twinStatusId}/v1', {
+  //         params: {
+  //             header: getApiDomainHeaders(settings),
+  //             path: {twinStatusId: statusId}
+  //         },
+  //         body: data
+  //     })
+  // }
 
-    function search({twinClassId, pagination, nameFilter, descriptionFilter, initialStatusFilter}: {
-        twinClassId: string,
-        pagination: PaginationState,
-        nameFilter?: string,
-        descriptionFilter?: string,
-        initialStatusFilter?: string,
-    }) {
+  function search({
+    twinClassId,
+    pagination,
+    nameFilter,
+    descriptionFilter,
+    initialStatusFilter,
+  }: {
+    twinClassId: string;
+    pagination: PaginationState;
+    nameFilter?: string;
+    descriptionFilter?: string;
+    initialStatusFilter?: string;
+  }) {
+    return settings.client.POST("/private/twinflow/search/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        query: {
+          showTwinflowMode: "DETAILED",
+          showTwinflow2TransitionMode: "DETAILED",
+          showTransition2StatusMode: "SHORT",
+          showTwinflowInitStatus2StatusMode: "SHORT",
+          offset: pagination.pageIndex * pagination.pageSize,
+          limit: pagination.pageSize,
+        },
+      },
+      body: {
+        twinClassIdList: [twinClassId],
+        nameI18nLikeList: nameFilter ? ["%" + nameFilter + "%"] : undefined,
+        descriptionI18nLikeList: descriptionFilter
+          ? ["%" + descriptionFilter + "%"]
+          : undefined,
+        initialStatusIdList: initialStatusFilter
+          ? [initialStatusFilter]
+          : undefined,
+      },
+    });
+  }
 
-        return settings.client.POST("/private/twinflow/search/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                query: {
-                    showTwinflowMode: 'DETAILED',
-                    showTwinflow2TransitionMode: 'DETAILED',
-                    showTransition2StatusMode: 'SHORT',
-                    showTwinflowInitStatus2StatusMode: 'SHORT',
-                    offset: pagination.pageIndex * pagination.pageSize,
-                    limit: pagination.pageSize
-                }
-            },
-            body: {
-                twinClassIdList: [twinClassId],
-                nameI18nLikeList: nameFilter ? ['%' + nameFilter + '%'] : undefined,
-                descriptionI18nLikeList: descriptionFilter ? ['%' + descriptionFilter + '%'] : undefined,
-                initialStatusIdList: initialStatusFilter ? [initialStatusFilter] : undefined,
-            }
-        })
-    }
+  function getById({ twinFlowId }: { twinFlowId: string }) {
+    return settings.client.GET("/private/twinflow/{twinflowId}/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinflowId: twinFlowId },
+        query: {
+          showTwinflowMode: "DETAILED",
+          showTwinflow2TransitionMode: "DETAILED",
+          showTransition2StatusMode: "SHORT",
+          showTwinflowInitStatus2StatusMode: "SHORT",
+        },
+      },
+    });
+  }
 
-    function getById({twinFlowId}: { twinFlowId: string }) {
-        return settings.client.GET("/private/twinflow/{twinflowId}/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinflowId: twinFlowId},
-                query: {
-                    showTwinflowMode: 'DETAILED',
-                    showTwinflow2TransitionMode: 'DETAILED',
-                    showTransition2StatusMode: 'SHORT',
-                    showTwinflowInitStatus2StatusMode: 'SHORT',
-                }
-            },
-        })
-    }
+  function create({
+    twinClassId,
+    body,
+  }: {
+    twinClassId: string;
+    body: TwinFlowCreateRq;
+  }) {
+    return settings.client.POST(
+      "/private/twin_class/{twinClassId}/twinflow/v1",
+      {
+        params: {
+          header: getApiDomainHeaders(settings),
+          path: { twinClassId },
+        },
+        body: body,
+      }
+    );
+  }
 
-    function create({twinClassId, body}: { twinClassId: string, body: TwinFlowCreateRq }) {
-        return settings.client.POST("/private/twin_class/{twinClassId}/twinflow/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinClassId}
-            },
-            body: body
-        })
-    }
+  function update({ id, body }: { id: string; body: TwinFlowUpdateRq }) {
+    return settings.client.PUT("/private/twinflow/{twinflowId}/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { twinflowId: id },
+      },
+      body,
+    });
+  }
 
-    function update({id, body}: { id: string, body: TwinFlowUpdateRq }) {
-        return settings.client.PUT("/private/twinflow/{twinflowId}/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinflowId: id}
-            },
-            body
-        })
-    }
+  function getTransitionById({ transitionId }: { transitionId: string }) {
+    return settings.client.GET("/private/transition/{transitionId}/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { transitionId },
+        query: {
+          showTransitionMode: "MANAGED",
+          showTransition2StatusMode: "DETAILED",
+        },
+      },
+    });
+  }
 
+  function createTransition({
+    twinFlowId,
+    data,
+  }: {
+    twinFlowId: string;
+    data: TwinFlowTransitionCreateRq;
+  }) {
+    return settings.client.POST(
+      "/private/twinflow/{twinflowId}/transition/v1",
+      {
+        params: {
+          header: getApiDomainHeaders(settings),
+          path: { twinflowId: twinFlowId },
+        },
+        body: data,
+      }
+    );
+  }
 
-    function getTransitionById({transitionId}: { transitionId: string }) {
-        return settings.client.GET("/private/transition/{transitionId}/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {transitionId},
-                query: {
-                    showTransitionMode: 'MANAGED',
-                    showTransition2StatusMode: 'DETAILED'
-                }
-            },
-        })
-    }
+  function updateTransition({
+    transitionId,
+    data,
+  }: {
+    transitionId: string;
+    data: TwinFlowTransitionUpdateRq;
+  }) {
+    return settings.client.POST("/private/transition/{transitionId}/v1", {
+      params: {
+        header: getApiDomainHeaders(settings),
+        path: { transitionId },
+      },
+      body: data,
+    });
+  }
 
-    function createTransition({twinFlowId, data}: { twinFlowId: string, data: TwinFlowTransitionCreateRq }) {
-        return settings.client.POST("/private/twinflow/{twinflowId}/transition/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {twinflowId: twinFlowId}
-            },
-            body: data
-        })
-    }
-
-    function updateTransition({transitionId, data}: { transitionId: string, data: TwinFlowTransitionUpdateRq }) {
-        return settings.client.POST("/private/transition/{transitionId}/v1", {
-            params: {
-                header: getApiDomainHeaders(settings),
-                path: {transitionId}
-            },
-            body: data
-        })
-    }
-
-    return {search, getById, create, update, getTransitionById, createTransition, updateTransition}
+  return {
+    search,
+    getById,
+    create,
+    update,
+    getTransitionById,
+    createTransition,
+    updateTransition,
+  };
 }
 
 export type TwinflowApi = ReturnType<typeof createTwinflowApi>;
