69a596202815dfae56da2556a76fb188228bc4d6
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-10-15 11:19:12 +0300

    [TWINFACES-61] created a links page to show forward and backward tables

--- src/app/twin/[twinId]/page.tsx
+++ src/app/twin/[twinId]/page.tsx
@@ -1,16 +1,27 @@
-"use client";
+'use client'
 
-import { Section, SideNavLayout } from "@/components/layout/side-nav-layout";
-import { TwinGeneral } from "@/app/twin/[twinId]/twin-general";
+import {Section, SideNavLayout} from "@/components/layout/side-nav-layout";
+import {TwinGeneral} from "@/app/twin/[twinId]/twin-general";
+import {TwinLinks} from "@/app/twin/[twinId]/twin-links";
 
 export default function TwinPage() {
-  const sections: Section[] = [
-    {
-      key: "general",
-      label: "General",
-      content: <TwinGeneral />,
-    },
-  ];
 
-  return <SideNavLayout sections={sections} />;
+    const sections: Section[] = [
+        {
+            key: 'general',
+            label: 'General',
+            content: <TwinGeneral/>
+        },
+        {
+            key: 'links',
+            label: 'Links',
+            content: <TwinLinks/>
+        },
+    ]
+
+    return <SideNavLayout sections={sections}/>
 }
+
+
+
+
