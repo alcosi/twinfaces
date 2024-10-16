3d6e75eecb66328d8a18736343ef56ab7490c221
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-10-16 16:20:33 +0300

    [TWINFACES-61] add twinID for search links

--- src/lib/api/api-groups/twin-api.ts
+++ src/lib/api/api-groups/twin-api.ts
@@ -64,7 +64,7 @@ export function createTwinApi(settings: ApiSettings) {
       body: body,
     });
   }
-    function searchLinks() {
+    function searchLinks({twinId}: { twinId: string }) {
         return settings.client.POST('/private/twin/search/v3', {
             params: {
                 header: getApiDomainHeaders(settings),
@@ -74,7 +74,9 @@ export function createTwinApi(settings: ApiSettings) {
                     limit: 10,
                 }
             },
-            body: [{}]
+            body: [{
+                twinIdList: [twinId]
+            }]
         })
     }
 
