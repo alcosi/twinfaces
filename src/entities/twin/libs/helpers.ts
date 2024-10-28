d9c94be35036822d544043572645c33ab915911b
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-10-28 11:29:08 +0300

    [TWINFACES-37] fixed filtering logic for the twin page

--- src/entities/twin/libs/helpers.ts
+++ src/entities/twin/libs/helpers.ts
@@ -12,13 +12,30 @@ const stringLikeFilters = [FilterFields.twinNameLikeList];
 
 const arrayLikeFilters = [
   FilterFields.twinIdList,
-  FilterFields.statusIdList,
-  FilterFields.assignerUserIdList,
   FilterFields.twinClassIdList,
+  FilterFields.statusIdList,
+  FilterFields.createdByUserIdList,
+  FilterFields.assignerUserIdList,
+  FilterFields.headTwinIdList,
+  FilterFields.tagDataListOptionIdList,
+  FilterFields.markerDataListOptionIdList,
 ];
 
 // List of filter keys for choice-mapping
 const choiceMappingFilters: any = [];
+
+const toArrayOfString = <T extends { [key: string]: any }>(
+  input: T[] | string[],
+  key: keyof T = "id"
+): string[] => {
+  if (input.every((item) => typeof item === "string")) {
+    return input as string[];
+  }
+
+  return (input as T[])
+    .map((item) => item[key] || "")
+    .filter((value) => value !== "");
+};
 
 export const buildFilters = (filters: FiltersState): Record<string, any> => {
   return Object.entries(FILTERS).reduce<Record<string, any>>(
@@ -29,17 +46,31 @@ export const buildFilters = (filters: FiltersState): Record<string, any> => {
       if (stringLikeFilters.includes(filterKey as FilterFields)) {
         acc[filterKey] = filterValue ? [`%${filterValue}%`] : [];
       } else if (arrayLikeFilters.includes(filterKey as FilterFields)) {
-        acc[filterKey] = toArray(filterValue);
+        const key = getKeyByFilter(filterKey);
+        acc[filterKey] = toArrayOfString(toArray(filterValue), key);
       } else if (choiceMappingFilters.includes(filterKey as FilterFields)) {
         acc[filterKey] = mapToChoice(filterValue);
       } else {
-        acc[filterKey] = filterValue;
+        acc[filterKey] = Array.isArray(filterValue)
+          ? toArrayOfString(filterValue)
+          : filterValue;
       }
 
       return acc;
     },
     {}
   );
+};
+
+const getKeyByFilter = (filterKey: string): string => {
+  switch (filterKey) {
+    case "twinClassIdList":
+      return "twinClassId";
+    case "statusIdList":
+      return "statusId";
+    default:
+      return "headTwinId";
+  }
 };
 
 export const hydrateTwinFromMap = (
