6d29318ee6458018d1f5cbf6854c13568f822f74
pihaizer <malhin.vladimir@gmail.com>
2024-11-20 11:54:30 +0100

    Remove extra code left by merges

--- src/entities/twinClass/libs/helpers.ts
+++ src/entities/twinClass/libs/helpers.ts
@@ -1,53 +1,6 @@
-import { FiltersState } from "@/components/base/data-table/crud-data-table";
 import { RelatedObjects } from "@/shared/api";
-import { mapToChoice, toArray, toArrayOfString } from "@/shared/libs";
 import { DataListsMap, TwinClass } from "../api";
-import { FilterFields, FILTERS } from "./constants";
 import { TwinClass_DETAILED } from "./types";
-
-
-const stringLikeFilters = [
-  FilterFields.twinClassKeyLikeList,
-  FilterFields.nameI18nLikeList,
-  FilterFields.descriptionI18nLikeList,
-];
-
-const arrayLikeFilters = [
-  FilterFields.twinClassIdList,
-  FilterFields.headTwinClassIdList,
-  FilterFields.extendsTwinClassIdList,
-  FilterFields.ownerTypeList,
-];
-
-const choiceMappingFilters = [
-  FilterFields.twinflowSchemaSpace,
-  FilterFields.twinClassSchemaSpace,
-  FilterFields.permissionSchemaSpace,
-  FilterFields.aliasSpace,
-  FilterFields.abstractt,
-];
-
-export const buildFilters = (filters: FiltersState): Record<string, any> => {
-  return Object.entries(FILTERS).reduce<Record<string, any>>(
-    (acc, [filterKey, _]) => {
-      const filterValue =
-        filters?.filters[filterKey as keyof FiltersState["filters"]];
-
-      if (stringLikeFilters.includes(filterKey as FilterFields)) {
-        acc[filterKey] = filterValue ? [`%${filterValue}%`] : [];
-      } else if (arrayLikeFilters.includes(filterKey as FilterFields)) {
-        acc[filterKey] = toArrayOfString(toArray(filterValue));
-      } else if (choiceMappingFilters.includes(filterKey as FilterFields)) {
-        acc[filterKey] = mapToChoice(filterValue);
-      } else {
-        acc[filterKey] = filterValue;
-      }
-
-      return acc;
-    },
-    {}
-  );
-};
 
 export const hydrateTwinClassFromMap = (
   twinClassDTO: TwinClass,
