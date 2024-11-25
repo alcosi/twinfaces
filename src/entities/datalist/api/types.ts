c244e9be55814488e9815b17f091aec549f8048e
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-11-25 09:34:57 +0300

    [TWINFACES-65] added logic for datalists filters

--- src/entities/datalist/api/types.ts
+++ src/entities/datalist/api/types.ts
@@ -3,7 +3,11 @@ import { components } from "@/shared/api/generated/schema";
 export type DataList = components["schemas"]["DataListV1"];
 export type DataListOption = components["schemas"]["DataListOptionV1"];
 
-export type DatalistFilterFields = "dataListIdList";
+export type DatalistFilterFields =
+  | "idList"
+  | "nameLikeList"
+  | "descriptionLikeList"
+  | "keyLikeList";
 
 export type DatalistApiFilters = Partial<
   Pick<components["schemas"]["DataListSearchRqV1"], DatalistFilterFields>
