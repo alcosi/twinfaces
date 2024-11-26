74f0a121bfaaf15150dd339042e70bc3bd5578f4
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-11-26 15:32:52 +0300

    [TWINFACES-114] changes after eslint

--- src/entities/datalist/libs/hooks/useFindDatalistById.ts
+++ src/entities/datalist/libs/hooks/useFindDatalistById.ts
@@ -23,9 +23,8 @@ export const useFetchDatalistById = () => {
         throw new Error(`Failed to find datalist with ID ${id}`);
       }
     },
-    [api],
+    [api]
   );
 
   return { fetchDatalistById };
 };
-
