4a5e26808e298b63cebe1795abaf0280720c560e
oleg ostrovski <oleg.ostrovski@gmail.com>
2025-01-03 14:48:40 +0300

    [TWINFACES-173] added useFetchFieldsById to get twin fields
    [TWINFACES-173] fix path for Experimental_CrudDataTable
    
    [TWINFACES-173] renamed useFetchFields
    
    TWINFACES-173 page twin tab fields

--- src/app/workspace/twin/[twinId]/views/twin-fields.tsx
+++ src/app/workspace/twin/[twinId]/views/twin-fields.tsx
@@ -1,24 +1,18 @@
-import { ApiContext, PagedResponse } from "@/shared/api";
-import {
-  CrudDataTable,
-  FiltersState,
-} from "@/shared/ui/data-table/crud-data-table";
+import { TwinField, useFetchFields } from "@/entities/twinField";
+import { PagedResponse } from "@/shared/api";
 import { DataTableHandle } from "@/shared/ui/data-table/data-table";
-import { ColumnDef, PaginationState } from "@tanstack/table-core";
+import { Experimental_CrudDataTable } from "@/widgets/crud-data-table";
+import { ColumnDef } from "@tanstack/table-core";
 import { useContext, useRef } from "react";
 import { toast } from "sonner";
 import { TwinContext } from "../twin-context";
 
-interface DataField {
-  [key: string]: string;
-}
-
 export function TwinFields() {
-  const api = useContext(ApiContext);
-  const { twin } = useContext(TwinContext);
+  const { twinId } = useContext(TwinContext);
   const tableRef = useRef<DataTableHandle>(null);
+  const { fetchFieldsByTwinId } = useFetchFields();
 
-  const columns: ColumnDef<DataField>[] = [
+  const columns: ColumnDef<TwinField>[] = [
     {
       id: "key",
       accessorKey: "key",
@@ -31,60 +25,24 @@ export function TwinFields() {
     },
   ];
 
-  async function fetchFields(
-    _: PaginationState,
-    filters: FiltersState
-  ): Promise<PagedResponse<{ key: string; value: string }>> {
-    if (!twin?.id) {
-      toast.error("Twin ID is missing");
-      return { data: [], pagination: {} };
-    }
-
+  async function fetchFields(): Promise<PagedResponse<TwinField>> {
     try {
-      const response = await api.twin.getFieldById({ fieldId: twin.id });
-      const data = response.data;
-
-      if (!data || data.status != 0) {
-        console.error("failed to fetch twin fields", data);
-        let message = "Failed to load twin fields";
-        if (data?.msg) message += `: ${data.msg}`;
-        toast.error(message);
-        return { data: [], pagination: {} };
-      }
-
-      const fields = data?.twin?.fields || [];
-      const entriesFields = Object.entries(fields).map(([key, value]) => ({
-        key,
-        value: value,
-      }));
-
-      return { data: entriesFields, pagination: {} };
+      const response = await fetchFieldsByTwinId({ twinId });
+      return response;
     } catch (e) {
-      console.error("exception while fetching twin fields", e);
       toast.error("Failed to fetch twin fields");
       return { data: [], pagination: {} };
     }
   }
 
-  if (!twin) {
-    console.error("TwinFields: no twin");
-    return;
-  }
-
   return (
-    <CrudDataTable
+    <Experimental_CrudDataTable
       ref={tableRef}
       title="Fields"
       columns={columns}
       getRowId={(row) => row.key!}
       fetcher={fetchFields}
-      // createButton={{
-      //     enabled: true,
-      //     onClick: createField,
-      // }}
       disablePagination={true}
-      pageSizes={[10, 20, 50]}
-      // onRowClick={(row) => router.push(`/workspace/twin/${twin!.id!}/twinField/${row.id}`)}
     />
   );
 }
