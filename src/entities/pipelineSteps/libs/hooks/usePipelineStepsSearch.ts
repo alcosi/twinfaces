fe0c7ac1712eed9bf15fd6dd8cd3d987072f52ca
DzianisKoval <d.koval@alcosi.eu>
2025-01-15 14:30:26 +0300

    [TWINFACES-324] feat: add hook for fetching pipelines steps date

--- src/entities/pipelineSteps/libs/hooks/usePipelineStepsSearch.ts
+++ src/entities/pipelineSteps/libs/hooks/usePipelineStepsSearch.ts
@@ -1,3 +1,42 @@
+import { useCallback, useContext } from "react";
+import { ApiContext, PagedResponse } from "@/shared/api";
+import { PaginationState } from "@tanstack/table-core";
+import { Factory, FactoryFilters } from "../../../factory/api/types";
+
 export function usePipelineStepsSearch() {
-  return console.log("pipelineSteps");
+  const api = useContext(ApiContext);
+
+  const searchPipelineSteps = useCallback(
+    async ({
+      pagination,
+      filters = {},
+    }: {
+      pagination: PaginationState;
+      filters?: FactoryFilters;
+    }): Promise<PagedResponse<Factory>> => {
+      try {
+        const { data, error } = await api.pipelineSteps.search({
+          pagination,
+          filters,
+        });
+
+        if (error) {
+          throw error;
+        }
+
+        return {
+          data: data.steps ?? [],
+          pagination: data.pagination ?? {},
+        };
+      } catch (error) {
+        console.error("Failed to fetch pipeline steps: ", error);
+        throw new Error(
+          "An error occured while fetching pipeline steps: " + error
+        );
+      }
+    },
+    [api]
+  );
+
+  return { searchPipelineSteps };
 }
