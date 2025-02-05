b7dd3766d1b878176c0bed507e577e72748c123c
Kerim Berdimyradov <k.berdimyradov@alcosi.eu>
2025-02-05 23:10:05 +0300

    TWINFACES-420 :bug: Fix assignee field (#185)

--- src/shared/libs/constants.ts
+++ src/shared/libs/constants.ts
@@ -9,7 +9,14 @@ export const REGEX_PATTERNS = {
   ALPHANUMERIC_WITH_DASHES: /^[a-zA-Z0-9_-]+$/,
 };
 
-export const FIRST_UUID_EXTRACTOR = z
+export const FIRST_ID_EXTRACTOR = z
   .array(z.object({ id: z.string().uuid("Please enter a valid UUID") }))
   .min(1, "Required")
   .transform((arr) => (isPopulatedArray<{ id: string }>(arr) ? arr[0].id : ""));
+
+export const FIRST_USER_ID_EXTRACTOR = z
+  .array(z.object({ userId: z.string().uuid("Please enter a valid UUID") }))
+  .min(1, "Required")
+  .transform((arr) =>
+    isPopulatedArray<{ userId: string }>(arr) ? arr[0].userId : ""
+  );
