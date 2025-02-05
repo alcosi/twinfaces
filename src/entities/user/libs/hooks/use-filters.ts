56593640a7bc4ec4914361e677e6d826ae4a3c8c
Kerim Berdimyradov <k.berdimyradov@alcosi.eu>
2025-02-05 15:44:09 +0300

    TWINFACES-0 :bug: Fixed [table] users filter

---
+++ src/entities/user/libs/hooks/use-filters.ts
@@ -0,0 +1,44 @@
+import { AutoFormValueInfo, AutoFormValueType } from "@/components/auto-field";
+import {
+  toArrayOfString,
+  wrapWithPercent,
+  type FilterFeature,
+} from "@/shared/libs";
+import { z } from "zod";
+import { DomainUserFilterKeys, DomainUserFilters } from "../../api";
+
+type Keys = Extract<DomainUserFilterKeys, "userIdList" | "nameLikeList">;
+type Filters = Pick<DomainUserFilters, "userIdList" | "nameLikeList">;
+
+export function useUserFilters(): FilterFeature<Keys, Filters> {
+  function buildFilterFields(): Record<Keys, AutoFormValueInfo> {
+    return {
+      userIdList: {
+        type: AutoFormValueType.tag,
+        label: "Id",
+        schema: z.string().uuid("Please enter a valid UUID"),
+        placeholder: "Enter UUID",
+      },
+
+      nameLikeList: {
+        type: AutoFormValueType.tag,
+        label: "Name",
+      },
+    } as const;
+  }
+
+  function mapFiltersToPayload(filters: Record<Keys, unknown>): Filters {
+    console.log("foobar map", filters);
+    const result: DomainUserFilters = {
+      userIdList: toArrayOfString(filters.userIdList),
+      nameLikeList: toArrayOfString(filters.nameLikeList).map(wrapWithPercent),
+    };
+
+    return result;
+  }
+
+  return {
+    buildFilterFields,
+    mapFiltersToPayload,
+  };
+}
