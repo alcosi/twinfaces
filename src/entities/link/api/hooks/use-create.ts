72f49e9797c3aff636a3be59785d64b586acfd54
DzianisKoval <d.koval@alcosi.eu>
2025-02-14 13:44:01 +0300

    [TWINFACES-458] feat: resolve some eslint warns

--- src/entities/link/api/hooks/use-create.ts
+++ src/entities/link/api/hooks/use-create.ts
@@ -1,9 +1,9 @@
-import { useCallback, useContext } from "react"
-import { z } from "zod"
-import { LINK_SCHEMA } from "../../libs"
-import { ApiContext } from "@/shared/api"
-import { toast } from "sonner"
-import { CreateLinkRequestBody } from "../types"
+import { useCallback, useContext } from "react";
+import { z } from "zod";
+import { LINK_SCHEMA } from "../../libs";
+import { ApiContext } from "@/shared/api";
+import { toast } from "sonner";
+import { CreateLinkRequestBody } from "../types";
 
 export const useCreateLink = () => {
   const api = useContext(ApiContext);
@@ -11,26 +11,27 @@ export const useCreateLink = () => {
   const createLink = useCallback(
     async (formValues: z.infer<typeof LINK_SCHEMA>) => {
       const body: CreateLinkRequestBody = {
-            forwardNameI18n: {
-              translations: {
-                en: formValues.name,
-              },
-            },
-            backwardNameI18n: {
-              translations: {
-                en: formValues.name,
-              },
-            },
-            ...formValues,
-          };
+        forwardNameI18n: {
+          translations: {
+            en: formValues.name,
+          },
+        },
+        backwardNameI18n: {
+          translations: {
+            en: formValues.name,
+          },
+        },
+        ...formValues,
+      };
 
-          const { error } = await api.link.create({ body });
-          if (error) {
-            throw error;
-          }
-          toast.success("Link created successfully!");
-    }, []
-  )
+      const { error } = await api.link.create({ body });
+      if (error) {
+        throw error;
+      }
+      toast.success("Link created successfully!");
+    },
+    []
+  );
 
   return { createLink };
-}
+};
