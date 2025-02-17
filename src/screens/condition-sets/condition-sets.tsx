c2deeb24449aa158c467a809abd5616cd094ec21
DzianisKoval <d.koval@alcosi.eu>
2025-02-17 10:48:48 +0300

    [TWINFACES-447] feat: add fetching data for table

--- src/screens/condition-sets/condition-sets.tsx
+++ src/screens/condition-sets/condition-sets.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import {
+  FactoryConditionSet,
+  useFactoryConditionSetSearch,
+} from "@/entities/factory-condition-set";
+import { GuidWithCopy } from "@/shared/ui";
+import { CrudDataTable, FiltersState } from "@/widgets/crud-data-table";
+import { ColumnDef, PaginationState } from "@tanstack/react-table";
+import { toast } from "sonner";
+
+const colDefs: Record<"id" | "name", ColumnDef<FactoryConditionSet>> = {
+  id: {
+    id: "id",
+    accessorKey: "id",
+    header: "ID",
+    cell: (data) => <GuidWithCopy value={data.getValue<string>()} />,
+  },
+  name: {
+    id: "name",
+    accessorKey: "name",
+    header: "Name",
+  },
+};
+
 export function ConditionSetsScreen() {
-  return <h1>ConditionSetScreen</h1>;
+  const { searchFactoryConditionSet } = useFactoryConditionSetSearch();
+
+  async function fetchFactoryConditionSet(
+    pagination: PaginationState,
+    filters: FiltersState
+  ) {
+    try {
+      return searchFactoryConditionSet({ pagination, filters: {} });
+    } catch (error) {
+      toast.error(
+        "An error occured while fetching factory condition sets: " + error
+      );
+      throw new Error(
+        "An error occured while fetching factory condition sets: " + error
+      );
+    }
+  }
+
+  return (
+    <CrudDataTable
+      columns={[colDefs.id, colDefs.name]}
+      fetcher={fetchFactoryConditionSet}
+      getRowId={(row) => row.id!}
+      defaultVisibleColumns={[colDefs.id, colDefs.name]}
+    />
+  );
 }
