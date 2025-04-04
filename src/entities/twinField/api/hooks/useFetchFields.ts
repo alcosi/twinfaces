a39c995db79e541ff1d7fc2fd97d09c85e1d9907
oleg ostrovski <oleg.ostrovski@gmail.com>
2025-04-04 14:41:56 +0300

    [TWINFACES-510] fix bag for twinField.descriptor.fieldType was undefined

--- src/entities/twinField/api/hooks/useFetchFields.ts
+++ src/entities/twinField/api/hooks/useFetchFields.ts
@@ -32,7 +32,6 @@ export const useFetchFields = () => {
         hydrateTwinFieldFromMap({
           dto,
           relatedObjects: data.relatedObjects,
-          twinClassId: data?.twin?.twinClassId,
         })
       );
 
--- src/entities/twinField/libs/helpers.ts
+++ src/entities/twinField/libs/helpers.ts
@@ -5,21 +5,19 @@ import { TwinFieldType, TwinFieldUI } from "./types";
 export const hydrateTwinFieldFromMap = ({
   dto,
   relatedObjects,
-  twinClassId,
 }: {
   dto: [string, string];
   relatedObjects?: RelatedObjects;
-  twinClassId?: string;
 }): TwinFieldUI => {
   const [key, value] = dto;
 
   const twinFieldKeyValue: TwinFieldUI = { key, value } as TwinFieldUI;
 
-  if (!relatedObjects?.twinClassMap || !twinClassId) return twinFieldKeyValue;
+  if (!relatedObjects?.twinClassFieldMap) return twinFieldKeyValue;
 
-  const twinClassField = (
-    relatedObjects.twinClassMap[twinClassId]?.fields ?? []
-  ).find((field) => field.key === key);
+  const twinClassField = Object.values(relatedObjects.twinClassFieldMap).find(
+    (field) => field.key === key
+  );
 
   // NOTE: To support preview state for diff `fieldType`(s)
   switch (twinClassField?.descriptor?.fieldType) {
