088b9cf4cf0b25022ae3c566b79fc139a45cb823
DzianisKoval <d.koval@alcosi.eu>
2025-04-07 15:13:11 +0300

    [TWINFACES-509] refactor: add system icon visible if selected

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -13,14 +13,24 @@ import {
 } from "@/shared/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+
+  const icons = {
+    light: (
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    ),
+    dark: (
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    ),
+    system: <Monitor className="h-[1.2rem] w-[1.2rem] cursor-pointer" />,
+  };
+  const selectedIcon = icons[theme as keyof typeof icons] ?? icons.light;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" className="relative">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {selectedIcon}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
