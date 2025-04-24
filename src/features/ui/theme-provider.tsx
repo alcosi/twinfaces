2c5228cb23a169b39eee5c4e318950de0a581045
DzianisKoval <d.koval@alcosi.eu>
2025-04-24 10:27:09 +0300

    [TWINFACES-530] Fix Hydration Errors and SVG Mismatch in LoginForm (#308)
    * [TWINFACES-530] fix: hydration error
    
    * Resolve PR
    
    ---------
    
    Co-authored-by: Kerim Berdimyradov <kberdimyradov@gmail.com>

--- src/features/ui/theme-provider.tsx
+++ src/features/ui/theme-provider.tsx
@@ -2,8 +2,13 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
-import * as React from "react";
+
+import { RenderOnClient } from "@/shared/ui";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <RenderOnClient>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </RenderOnClient>
+  );
 }
