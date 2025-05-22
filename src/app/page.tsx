ccd0868f080eca32a192ce3cf338c74aaae63153
Kerim Berdimyradov <kberdimyradov@gmail.com>
2025-05-22 14:18:01 +0300

    TWINFACES-513 :sparkles: handle domain header and redirect to '/auth'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { DomainSelector } from "@/screens/domain-selector";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+import { getDomainFromHeaders } from "@/entities/face";
+import { DomainSelector } from "@/screens/domain-selector";
+import { isPopulatedString } from "@/shared/libs";
+
+export default async function Page() {
+  const remoteConfig = await getDomainFromHeaders();
+
+  if (isPopulatedString(remoteConfig?.id)) {
+    redirect(`/auth?domainId=${encodeURIComponent(remoteConfig.id)}`);
+  }
+
   return <DomainSelector />;
 }
