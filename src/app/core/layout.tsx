9d9496812474526d7566c4dbb5a93fa1c612684a
oleg ostrovski <oleg.ostrovski@gmail.com>
2025-06-10 17:24:59 +0300

    [TWINFACES-570] added protected routes logic for user not admin

--- src/app/core/layout.tsx
+++ src/app/core/layout.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import { getAuthHeaders } from "@/entities/face";
+import { KEY_TO_ID_PERMISSION_MAP } from "@/entities/permission/server";
+import { isGranted } from "@/entities/user/server";
+import { PermissionDeniedScreen } from "@/screens/permission-denied";
 import { PrivateLayoutProviders } from "@/widgets/layout";
 
-export default function Layout({
+export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const { currentUserId } = await getAuthHeaders();
+  const isAdmin = await isGranted({
+    userId: currentUserId,
+    permission: KEY_TO_ID_PERMISSION_MAP.DOMAIN_MANAGE,
+  });
+
+  if (!isAdmin) {
+    return <PermissionDeniedScreen />;
+  }
+
   return <PrivateLayoutProviders>{children}</PrivateLayoutProviders>;
 }
