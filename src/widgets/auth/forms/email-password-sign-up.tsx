b2bf2cd1926bd26d572433fb184c883d57b8eb75
Maksim <106744547+Bulrock@users.noreply.github.com>
2025-06-13 11:40:31 +0200

    TWINFACES-574 refactor: on failed login or signup keep form fields filled (clear password field for signin form on failed) (#355)
    * TWINFACES-574 refactor: on failed login or signup keep form fields filled (clear password field for signin form on failed)
    
    * TWINFACES-574 refactor: remove env file
    
    * Resolve PR
    
    ---------
    
    Co-authored-by: Kerim Berdimyradov <kberdimyradov@gmail.com>

--- src/widgets/auth/forms/email-password-sign-up.tsx
+++ src/widgets/auth/forms/email-password-sign-up.tsx
@@ -72,7 +72,8 @@ export function EmailPasswordSignUpForm({
           err instanceof Error ? err.message : "An unexpected error occurred."
         );
         onError?.();
-        singUpForm.reset();
+        singUpForm.resetField("password");
+        singUpForm.resetField("confirmPassword");
       }
     });
   }
