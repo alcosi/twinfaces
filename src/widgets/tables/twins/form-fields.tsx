133d52845fa5283309b821d5a009fec41c74d414
Maksim Volozhinskij <m.valazhynski@alcosi.eu>
2025-06-23 14:37:06 +0200

    TWINFACES-580 refactor: filter twins form fields

--- src/widgets/tables/twins/form-fields.tsx
+++ src/widgets/tables/twins/form-fields.tsx
@@ -11,6 +11,8 @@ import { TwinFormValues } from "@/entities/twin";
 import { TwinFieldFormField } from "../../form-fields";
 import { useTwinClassFields } from "./use-twin-form-fields";
 
+const EXCLUDED_FIELD_IDS = ["f55fba6d-9cf4-4011-8e62-5e75992f923c"];
+
 export function TwinFormFields({
   control,
   baseTwinClassId,
@@ -22,7 +24,7 @@ export function TwinFormFields({
     selectedTwinClass,
     twinClassAdapter,
     fields,
-    userAdapter,
+    // userAdapter,
     hasHeadClass,
     headAdapter,
     hasTagDataList,
@@ -34,10 +36,10 @@ export function TwinFormFields({
       <ComboboxFormField
         control={control}
         name="classId"
-        label="Class"
-        selectPlaceholder="Select twin class"
-        searchPlaceholder="Search twin class..."
-        noItemsText={"No classes found"}
+        label="Marketplace"
+        selectPlaceholder="Select marketplace"
+        searchPlaceholder="Search marketplace..."
+        noItemsText={"No marketplaces found"}
         {...twinClassAdapter}
       />
 
@@ -59,24 +61,26 @@ export function TwinFormFields({
         label="Description"
       />
 
-      <ComboboxFormField
+      {/* <ComboboxFormField
         name="assignerUserId"
         control={control}
         label="Assignee"
         {...userAdapter}
-      />
+      /> */}
 
-      {fields.map((field) => (
-        <TwinFieldFormField
-          key={field.key}
-          name={`fields.${field.key!}`}
-          control={control}
-          label={field.name}
-          twinClassId={selectedTwinClass!.id}
-          descriptor={field.descriptor}
-          required={field.required}
-        />
-      ))}
+      {fields
+        .filter((field) => !EXCLUDED_FIELD_IDS.includes(field?.id!))
+        .map((field) => (
+          <TwinFieldFormField
+            key={field.key}
+            name={`fields.${field.key!}`}
+            control={control}
+            label={field.name}
+            twinClassId={selectedTwinClass!.id}
+            descriptor={field.descriptor}
+            required={field.required}
+          />
+        ))}
 
       {hasTagDataList && (
         <ComboboxFormField
