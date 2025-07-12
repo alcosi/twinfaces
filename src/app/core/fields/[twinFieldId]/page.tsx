2c282861e26e2cd9b0a8b0c1c255b8bddd45950b
DzianisKoval <d.koval@alcosi.eu>
2025-07-12 16:42:17 +0300

    [TWINFACES-436] feat: resolve TODO

--- src/app/core/fields/[twinFieldId]/page.tsx
+++ src/app/core/fields/[twinFieldId]/page.tsx
@@ -1,11 +1,8 @@
 import { Suspense } from "react";
 
-import { getAuthHeaders } from "@/entities/face";
-import { hydrateTwinClassFieldFromMap } from "@/entities/twin-class-field/libs/helpers";
+import { fetchTwinClassFieldById } from "@/entities/twin-class-field/server";
 import { LoadingScreen } from "@/screens/loading";
 import { TwinClassFieldScreen } from "@/screens/twin-class-field";
-import { TwinsAPI } from "@/shared/api";
-import { isUndefined } from "@/shared/libs";
 
 type Props = {
   params: {
@@ -21,38 +18,4 @@ export default async function Page({ params: { twinFieldId } }: Props) {
       <TwinClassFieldScreen twinFieldId={twinFieldId} twinField={twinField} />
     </Suspense>
   );
-}
-
-async function fetchTwinClassFieldById(id: string) {
-  const header = await getAuthHeaders();
-
-  const { data, error } = await TwinsAPI.GET(
-    "/private/twin_class_field/{twinClassFieldId}/v1",
-    {
-      params: {
-        header,
-        path: { twinClassFieldId: id },
-        query: {
-          lazyRelation: false,
-          showTwinClassFieldMode: "MANAGED",
-          showTwinClass2PermissionMode: "DETAILED",
-          showTwinClassField2TwinClassMode: "DETAILED",
-          showTwinClassField2FeaturerMode: "DETAILED",
-          showTwinClassField2PermissionMode: "DETAILED",
-        },
-      },
-    }
-  );
-
-  if (error) {
-    throw error;
-  }
-
-  if (isUndefined(data?.field)) {
-    throw new Error("Response does not have twin-class-field data", error);
-  }
-
-  const field = hydrateTwinClassFieldFromMap(data.field, data.relatedObjects);
-
-  return field;
 }
--- src/entities/twin-class-field/server/index.ts
+++ src/entities/twin-class-field/server/index.ts
@@ -1,3 +1,1 @@
-// TODO: move this this folder
-// async function fetchTwinClassFieldById(id: string)
-// from src/app/core/fields/[twinFieldId]/page.tsx
+export * from "./api";
