4bceec0251804518340d1da42f1ddd13c34fe74d
oleg ostrovski <oleg.ostrovski@gmail.com>
2025-07-24 13:46:57 +0300

    [TWINFACES-612] fixed logic for correct transition to the general page

--- src/app/core/statuses/[twinStatusId]/page.tsx
+++ src/app/core/statuses/[twinStatusId]/page.tsx
@@ -1,26 +1,20 @@
-"use client";
-
-import { ReactNode, use } from "react";
-
 import { TwinStatusContextProvider } from "@/features/twin-status";
+import { StatusScreen } from "@/screens/status";
 
 type Props = {
   params: Promise<{
     twinStatusId: string;
   }>;
-  children: ReactNode;
 };
 
-export default function Layout(props: Props) {
-  const params = use(props.params);
+export default async function Page(props: Props) {
+  const params = await props.params;
 
   const { twinStatusId } = params;
 
-  const { children } = props;
-
   return (
     <TwinStatusContextProvider twinStatusId={twinStatusId}>
-      {children}
+      <StatusScreen />
     </TwinStatusContextProvider>
   );
 }
