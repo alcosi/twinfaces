6888317f71e9990339438b87b019259e3918da93
Irina Lash <i.lashitskaya@alcosi.eu>
2025-07-29 12:30:27 +0300

    [TWINFACES-589] fix comments

--- src/widgets/faces/widgets/views/tw004/tw004.tsx
+++ src/widgets/faces/widgets/views/tw004/tw004.tsx
@@ -3,7 +3,7 @@ import { KEY_TO_ID_PERMISSION_MAP } from "@/entities/permission/server";
 import { isAuthUserGranted } from "@/entities/user/server";
 import { withRedirectOnUnauthorized } from "@/features/auth";
 import { TwinFieldEditor } from "@/features/twin/ui/field-editor";
-import { cn, isFalsy, isMultiElementArray, safe } from "@/shared/libs";
+import { cn, isMultiElementArray, safe } from "@/shared/libs";
 
 import { StatusAlert } from "../../../components";
 import { TWidgetFaceProps } from "../../types";
@@ -43,44 +43,44 @@ export async function TW004(props: TWidgetFaceProps) {
     })
   );
 
+  const renderedFields = sortedFields.map((el, index) => {
+    if (
+      fieldEditorPropResults[index]?.status === "fulfilled" &&
+      fieldEditorPropResults[index].value.ok
+    ) {
+      const { twin, relatedObjects, field } =
+        fieldEditorPropResults[index]?.value.data;
+
+      return (
+        <TwinFieldEditor
+          id={id!}
+          label={el.label || "N/A"}
+          twinId={twidget.pointedTwinId!}
+          twin={twin}
+          relatedObjects={relatedObjects}
+          field={field}
+          disabled={!isAdmin}
+          editable={el.editable}
+        />
+      );
+    } else
+      return (
+        <StatusAlert
+          variant="error"
+          title={name}
+          message={`Face with id ${id} failed to load`}
+          className="mt-4"
+        />
+      );
+  });
+
   return (
     <div data-face-id={id} className={cn(className)}>
       {isMultiElementArray(fields) && label && (
-        <span className="text-s font-bold">{label}</span>
+        <span className="text-base font-bold">{label}</span>
       )}
       <div className={cn(className, twidget.styleClasses)}>
-        {sortedFields.map((el, index) => {
-          const elementResult =
-            fieldEditorPropResults[index]?.status === "fulfilled" &&
-            fieldEditorPropResults[index].value.ok
-              ? fieldEditorPropResults[index].value.data
-              : false;
-
-          if (isFalsy(elementResult))
-            return (
-              <StatusAlert
-                variant="error"
-                title={name}
-                message={`Face with id ${id} failed to load`}
-                className="mt-4"
-              />
-            );
-
-          const { twin, relatedObjects, field } = elementResult;
-
-          return (
-            <TwinFieldEditor
-              id={id!}
-              label={el.label || "N/A"}
-              twinId={twidget.pointedTwinId!}
-              twin={twin}
-              relatedObjects={relatedObjects}
-              field={field}
-              disabled={!isAdmin}
-              editable={el.editable}
-            />
-          );
-        })}
+        {renderedFields}
       </div>
     </div>
   );
