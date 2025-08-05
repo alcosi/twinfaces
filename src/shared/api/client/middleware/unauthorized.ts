3c1ded968c001f8916b86bbbc62065c5d982e8b0
Irina Lash <i.lashitskaya@alcosi.eu>
2025-08-05 10:36:00 +0300

    [TWINFACES-601] fix redirect

--- src/shared/api/client/middleware/unauthorized.ts
+++ src/shared/api/client/middleware/unauthorized.ts
@@ -1,13 +1,14 @@
+import { redirect } from "next/navigation";
 import { Middleware } from "openapi-fetch";
 
-import { isBrowserRuntime, isUnauthorizedError } from "@/shared/libs";
+import { isUnauthorizedError } from "@/shared/libs";
 
 import { EXPIRED_SESSION_TAG } from "../client";
 
 export const unauthorizedMiddleware: Middleware = {
   async onResponse({ response }) {
-    if (isUnauthorizedError(response) && isBrowserRuntime()) {
-      window.location.href = `/?reason=${EXPIRED_SESSION_TAG}`;
+    if (isUnauthorizedError(response)) {
+      redirect(`/?reason=${EXPIRED_SESSION_TAG}`);
     }
 
     return response;
