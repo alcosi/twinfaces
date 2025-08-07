2c0037c6f59cf767ca5ccd9327950af882786b9e
Denis <d.koval@alcosi.eu>
2025-08-07 19:56:06 +0300

    [SHLVS-587] [FACES] Enhance WT001 To support Navigation On Click (#421)
    * [SHLVS-587] feat: add browse slug, and implement fetching current twin by twinId
    
    * [SHLVS-587] feat: resolve PR
    
    * [SHLVS-587] feat: revert some changes
    
    * [SHLVS-587] feat: revert some changes
    
    * [SHLVS-587] feat: refactor
    
    * [SHLVS-587] feat: refactor
    
    * PR resolve
    
    * Resolve PR
    
    ---------
    
    Co-authored-by: Kerim Berdimyradov <kberdimyradov@gmail.com>

--- src/shared/libs/utils/safe.ts
+++ src/shared/libs/utils/safe.ts
@@ -1,11 +1,17 @@
 import { Result } from "@/shared/api";
 
-export async function safe<T>(fn: () => Promise<T>): Promise<Result<T>> {
+import { isErrorInstance } from "../types";
+
+export async function safe<T>(fn: () => Promise<T>): Promise<Result<T, Error>> {
   try {
     const data = await fn();
     return { ok: true, data };
   } catch (error) {
     console.warn("[safe] Caught error:", error);
-    return { ok: false, error };
+
+    return {
+      ok: false,
+      error: isErrorInstance(error) ? error : new Error("Unknown Error"),
+    };
   }
 }
--- src/widgets/faces/widgets/views/wt001/wt001-client.tsx
+++ src/widgets/faces/widgets/views/wt001/wt001-client.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import { FaceTC001ViewRs as FaceTC, FaceWT001 } from "@/entities/face";
+import { Twin_DETAILED } from "@/entities/twin/server";
+import { PlatformArea } from "@/shared/config";
 
 import { TwinsTable } from "../../../../tables";
 
@@ -21,6 +25,12 @@ export function WT001Client<T extends FaceTC = FaceTC>({
   isAdmin,
   modalCreateData,
 }: WT001ClientProps<T>) {
+  const router = useRouter();
+
+  function handleRowClick(row: Twin_DETAILED) {
+    router.push(`/${PlatformArea.browse}/${row.id}`);
+  }
+
   return (
     <TwinsTable
       title={title}
@@ -29,6 +39,7 @@ export function WT001Client<T extends FaceTC = FaceTC>({
       showCreateButton={showCreateButton}
       resourceNavigationEnabled={isAdmin}
       modalCreateData={modalCreateData}
+      onRowClick={handleRowClick}
     />
   );
 }
