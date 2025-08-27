4719212a1025f02390162956aaf093cee2c5db9c
Kerim Berdimyradov <kberdimyradov@gmail.com>
2025-08-27 17:27:58 +0300

    WIP: Test

--- src/shared/api/client/client.ts
+++ src/shared/api/client/client.ts
@@ -4,7 +4,7 @@ import createClient from "openapi-fetch";
 import { paths } from "@/shared/api/generated/schema";
 
 import { errorMiddleware } from "./middleware/error";
-import { refreshTokenMiddleware } from "./middleware/refresh-token";
+import { tryToRefreshTokenMiddleware } from "./middleware/refresh-token";
 import { unauthorizedMiddleware } from "./middleware/unauthorized";
 
 export const EXPIRED_SESSION_TAG = "session_expired";
@@ -13,8 +13,17 @@ export const TwinsAPI = createClient<paths>({
   baseUrl: env("NEXT_PUBLIC_TWINS_API_URL"),
 });
 
-// NOTE: order matters: normalize errors first, then handle 401s
-TwinsAPI.use(errorMiddleware);
-// NOTE: If auth token expires in <= treshholdMinutes, it will be refreshed before the request
-TwinsAPI.use(refreshTokenMiddleware(1));
+/*
+ The order in which middleware are registered matters. 
+ 
+ For requests, onRequest() will be called in the order registered.
+ For responses, onResponse() will be called in reverse order. 
+ 
+ That way the first middleware gets the first “dibs” on requests,
+ and the final control over the end response.
+
+ see: https://openapi-ts.dev/openapi-fetch/middleware-auth
+*/
+TwinsAPI.use(errorMiddleware); // normalize errors first, then handle 401s
+TwinsAPI.use(tryToRefreshTokenMiddleware());
 TwinsAPI.use(unauthorizedMiddleware);
--- src/shared/api/client/middleware/refresh-token.ts
+++ src/shared/api/client/middleware/refresh-token.ts
@@ -1,48 +1,74 @@
 import { Middleware } from "openapi-fetch";
 
-import { isServerRuntime } from "@/shared/libs";
+// import { isServerRuntime } from "@/shared/libs";
 
-import { IGNORED_PATHS, getCookie, getFreshToken } from "./utils";
+// import { IGNORED_PATHS, getCookie, getFreshToken } from "./utils";
 
-export function refreshTokenMiddleware(treshholdMinutes = 10): Middleware {
+const ms = 5 * 60_000;
+
+export function tryToRefreshTokenMiddleware(thresholdMs?: number): Middleware {
   let refreshingPromise: Promise<string | null> | null = null;
 
   return {
-    async onRequest({ request }) {
-      const url = new URL(request.url);
-      if (IGNORED_PATHS.includes(url.pathname)) return request;
+    // async onRequest({ request }) {
+    //   const url = new URL(request.url);
+    //   if (IGNORED_PATHS.includes(url.pathname)) return request;
 
-      if (isServerRuntime()) return request;
+    //   if (isServerRuntime()) return request;
 
-      let authToken = getCookie("authToken");
-      const refreshToken = getCookie("refreshToken");
-      const authTokenExpiresAt = getCookie("authTokenExpiresAt");
-      const domainId = getCookie("domainId");
+    //   let authToken = getCookie("authToken");
+    //   const refreshToken = getCookie("refreshToken");
+    //   const authTokenExpiresAt = getCookie("authTokenExpiresAt");
+    //   const domainId = getCookie("domainId");
 
-      if (refreshToken && authTokenExpiresAt && domainId) {
-        try {
-          const expiresAt = new Date(decodeURIComponent(authTokenExpiresAt));
-          const diffMinutes = (expiresAt.getTime() - Date.now()) / 1000 / 60;
+    //   if (refreshToken && authTokenExpiresAt && domainId) {
+    //     try {
+    //       const expiresAt = new Date(decodeURIComponent(authTokenExpiresAt));
+    //       const diffMinutes = (expiresAt.getTime() - Date.now()) / 60_000;
 
-          if (diffMinutes <= treshholdMinutes) {
-            if (!refreshingPromise) {
-              refreshingPromise = getFreshToken().finally(
-                () => (refreshingPromise = null)
-              );
-            }
+    //       if (diffMinutes <= thresholdMs) {
+    //         if (!refreshingPromise) {
+    //           refreshingPromise = getFreshToken().finally(
+    //             () => (refreshingPromise = null)
+    //           );
+    //         }
 
-            await refreshingPromise;
-            authToken = getCookie("authToken");
-          }
-        } catch (error) {
-          console.error("refreshTokenMiddleware error", error);
-        }
-      }
+    //         await refreshingPromise;
+    //         authToken = getCookie("authToken");
+    //       }
+    //     } catch (error) {
+    //       console.error("refreshTokenMiddleware error", error);
+    //     }
+    //   }
 
-      if (authToken) request.headers.set("AuthToken", authToken);
-      if (domainId) request.headers.set("DomainId", domainId);
+    //   if (authToken) request.headers.set("AuthToken", authToken);
+    //   if (domainId) request.headers.set("DomainId", domainId);
 
-      return request;
+    //   return request;
+    // },
+
+    // add to returned middleware object
+    async onResponse({ response, request }) {
+      console.log(`foobar onResponse#${response.status}`, {
+        response,
+        request,
+      });
+      // if (response.status === 401 || response.status === 419) {
+      //   if (!refreshingPromise) {
+      //     refreshingPromise = getFreshToken().finally(
+      //       () => (refreshingPromise = null)
+      //     );
+      //   }
+      //   await refreshingPromise;
+      //   const token = getCookie("authToken");
+      //   if (token) {
+      //     const headers = new Headers(request.headers);
+      //     headers.set("AuthToken", token);
+      //     const retried = new Request(request, { headers });
+      //     return client.fetch(retried);
+      //   }
+      // }
+      return response;
     },
   };
 }
