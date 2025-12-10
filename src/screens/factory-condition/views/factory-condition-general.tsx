a85f2bad92830495a9f07caad3c0bcdfc79e529a
kalmykov eugene <kalmykoveugene@gmail.com>
2025-12-10 18:32:26 +0300

    added create modal and update condition

--- src/screens/factory-condition/views/factory-condition-general.tsx
+++ src/screens/factory-condition/views/factory-condition-general.tsx
@@ -1,10 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { z } from "zod";
 
+import { AutoDialog, AutoEditDialogSettings } from "@/components/auto-dialog";
+import { AutoFormValueType } from "@/components/auto-field";
+
+import { useUpdateFactoryCondition } from "@/entities/factory-condition";
 import { FactoryConditionSet_DETAILED } from "@/entities/factory-condition-set";
-import { Featurer_DETAILED } from "@/entities/featurer";
+import {
+  Featurer_DETAILED,
+  useFeaturerSelectAdapter,
+} from "@/entities/featurer";
 import { FactoryConditionContext } from "@/features/factory-condition";
 import { FactoryConditionSetResourceLink } from "@/features/factory-condition-set/ui";
 import { FeaturerResourceLink } from "@/features/featurer/ui";
+import {
+  InPlaceEdit,
+  InPlaceEditContextProvider,
+  InPlaceEditProps,
+} from "@/features/inPlaceEdit";
 import {
   GuidWithCopy,
   Table,
@@ -14,67 +27,190 @@ import {
 } from "@/shared/ui";
 
 export function FactoryConditionGeneral() {
-  const { factoryCondition } = useContext(FactoryConditionContext);
+  const { factoryCondition, refresh } = useContext(FactoryConditionContext);
+  const featurerAdapter = useFeaturerSelectAdapter(24);
+  const { updateFactoryCondition } = useUpdateFactoryCondition();
+  const [editFieldDialogOpen, setEditFieldDialogOpen] = useState(false);
+  const [currentAutoEditDialogSettings, setCurrentAutoEditDialogSettings] =
+    useState<AutoEditDialogSettings | undefined>(undefined);
+
+  const conditionerFeaturerSettings: AutoEditDialogSettings = {
+    value: {
+      conditionerFeatureId: factoryCondition.conditionerFeaturerId || undefined,
+    },
+    title: "Update conditioner",
+    onSubmit: (values) => {
+      return updateFactoryCondition({
+        body: {
+          conditions: [
+            {
+              id: factoryCondition.id,
+              conditionerFeatureId: values.conditionerFeatureId[0].id,
+              conditionerParams: values.conditionerParams,
+            },
+          ],
+        },
+      }).then(refresh);
+    },
+    valuesInfo: {
+      conditionerFeatureId: {
+        type: AutoFormValueType.featurer,
+        label: "Conditioner",
+        typeId: 24,
+        paramsFieldName: "conditionerParams",
+        ...featurerAdapter,
+      },
+    },
+  };
+
+  const descriptionSettings: InPlaceEditProps<
+    typeof factoryCondition.description
+  > = {
+    id: "description",
+    value: factoryCondition.description,
+    valueInfo: {
+      type: AutoFormValueType.string,
+      input_props: {
+        fieldSize: "sm",
+      },
+      label: "",
+    },
+    schema: z.string().optional(),
+    onSubmit: async (value) => {
+      return updateFactoryCondition({
+        body: {
+          conditions: [
+            {
+              id: factoryCondition.id,
+              description: value,
+            },
+          ],
+        },
+      }).then(refresh);
+    },
+  };
+
+  const activeSettings: InPlaceEditProps<typeof factoryCondition.active> = {
+    id: "active",
+    value: factoryCondition.active,
+    valueInfo: {
+      type: AutoFormValueType.boolean,
+      label: "",
+    },
+    schema: z.boolean(),
+    renderPreview: (value) => (value ? "Yes" : "No"),
+    onSubmit: async (value) => {
+      return updateFactoryCondition({
+        body: {
+          conditions: [
+            {
+              id: factoryCondition.id,
+              active: value,
+            },
+          ],
+        },
+      }).then(refresh);
+    },
+  };
+
+  const invertSettings: InPlaceEditProps<typeof factoryCondition.invert> = {
+    id: "invert",
+    value: factoryCondition.invert,
+    valueInfo: {
+      type: AutoFormValueType.boolean,
+      label: "",
+    },
+    schema: z.boolean(),
+    renderPreview: (value) => (value ? "Yes" : "No"),
+    onSubmit: async (value) => {
+      return updateFactoryCondition({
+        body: {
+          conditions: [
+            {
+              id: factoryCondition.id,
+              invert: value,
+            },
+          ],
+        },
+      }).then(refresh);
+    },
+  };
+
+  function openWithSettings(settings: AutoEditDialogSettings) {
+    setCurrentAutoEditDialogSettings(settings);
+    setEditFieldDialogOpen(true);
+  }
 
   return (
-    <Table className="mt-8">
-      <TableBody>
-        <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell>
-            <GuidWithCopy value={factoryCondition.id} variant="long" />
-          </TableCell>
-        </TableRow>
+    <InPlaceEditContextProvider>
+      <Table className="mt-8">
+        <TableBody>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>
+              <GuidWithCopy value={factoryCondition.id} variant="long" />
+            </TableCell>
+          </TableRow>
 
-        <TableRow>
-          <TableCell>Factory Condition Set</TableCell>
-          <TableCell>
-            {factoryCondition.factoryConditionSet ? (
-              <FactoryConditionSetResourceLink
-                data={
-                  factoryCondition.factoryConditionSet as FactoryConditionSet_DETAILED
-                }
-                withTooltip
-              />
-            ) : (
-              "-"
-            )}
-          </TableCell>
-        </TableRow>
+          <TableRow>
+            <TableCell>Condition Set</TableCell>
+            <TableCell>
+              {factoryCondition.factoryConditionSet && (
+                <FactoryConditionSetResourceLink
+                  data={
+                    factoryCondition.factoryConditionSet as FactoryConditionSet_DETAILED
+                  }
+                  withTooltip
+                />
+              )}
+            </TableCell>
+          </TableRow>
 
-        <TableRow>
-          <TableCell>Conditioner Featurer</TableCell>
-          <TableCell>
-            {factoryCondition.conditionerFeaturer && (
-              <FeaturerResourceLink
-                data={factoryCondition.conditionerFeaturer as Featurer_DETAILED}
-                withTooltip
-              />
-            )}
-          </TableCell>
-        </TableRow>
+          <TableRow
+            className="cursor-pointer"
+            onClick={() => openWithSettings(conditionerFeaturerSettings)}
+          >
+            <TableCell>Conditioner Featurer</TableCell>
+            <TableCell>
+              {factoryCondition.conditionerFeaturer && (
+                <FeaturerResourceLink
+                  data={
+                    factoryCondition.conditionerFeaturer as Featurer_DETAILED
+                  }
+                  withTooltip
+                />
+              )}
+            </TableCell>
+          </TableRow>
 
-        <TableRow>
-          <TableCell>Description</TableCell>
-          <TableCell>
-            {factoryCondition.description && (
-              <div className="text-muted-foreground">
-                {factoryCondition.description}
-              </div>
-            )}
-          </TableCell>
-        </TableRow>
+          <TableRow>
+            <TableCell>Description</TableCell>
+            <TableCell>
+              <InPlaceEdit {...descriptionSettings} />
+            </TableCell>
+          </TableRow>
 
-        <TableRow>
-          <TableCell>Active</TableCell>
-          <TableCell>{factoryCondition.active ? "Yes" : "No"}</TableCell>
-        </TableRow>
+          <TableRow>
+            <TableCell>Active</TableCell>
+            <TableCell>
+              <InPlaceEdit {...activeSettings} />
+            </TableCell>
+          </TableRow>
 
-        <TableRow>
-          <TableCell>Invert</TableCell>
-          <TableCell>{factoryCondition.invert ? "Yes" : "No"}</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          <TableRow>
+            <TableCell>Invert</TableCell>
+            <TableCell>
+              <InPlaceEdit {...invertSettings} />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <AutoDialog
+        open={editFieldDialogOpen}
+        onOpenChange={setEditFieldDialogOpen}
+        settings={currentAutoEditDialogSettings}
+      />
+    </InPlaceEditContextProvider>
   );
 }
