9ec9ec2adc9635aff7c8ba555a5e16599a5a4933
Eugene-Kalmykov <piratsilver13@gmail.com>
2026-03-17 10:56:03 +0300

    Update [table filter] space roles

--- src/entities/space-role/libs/hooks/use-filters.ts
+++ src/entities/space-role/libs/hooks/use-filters.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { AutoFormValueInfo, AutoFormValueType } from "@/components/auto-field";
 
+import { useBusinessAccountSelectAdapter } from "@/entities/business-account";
 import {
   useTwinClassFilters,
   useTwinClassSelectAdapterWithFilters,
@@ -22,6 +23,7 @@ export function useSpaceRoleFilters({
   enabledFilters?: SpaceRoleFilterKeys[];
 } = {}): FilterFeature<SpaceRoleFilterKeys, SpaceRoleFilter> {
   const twinClassAdapter = useTwinClassSelectAdapterWithFilters();
+  const businessAccountAdapter = useBusinessAccountSelectAdapter();
 
   const {
     buildFilterFields: buildTwinClassFilters,
@@ -50,12 +52,10 @@ export function useSpaceRoleFilters({
       selectPlaceholder: "Select...",
       multi: true,
     },
-    // todo change when it will be unlocked https://alcosi.atlassian.net/browse/TWINFACES-835
     businessAccountIdList: {
-      type: AutoFormValueType.tag,
+      type: AutoFormValueType.combobox,
       label: "Business account",
-      schema: z.string().uuid("Please enter a valid UUID"),
-      placeholder: "Enter UUID",
+      ...businessAccountAdapter,
     },
     nameI18nLikeList: {
       type: AutoFormValueType.tag,
